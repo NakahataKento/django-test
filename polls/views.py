8436fe43c2fe767b20332d8b2793f3bddf2b5286
NakahataKento <kennto0716@gmail.com>
2019-11-06 17:43:15 +0900

    change1

--- polls/views.py
+++ polls/views.py
@@ -5,4 +5,4 @@
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello, Kento. You're at the polls index.")
+	return HttpResponse("Hello, Polls. You're at the polls index.")
